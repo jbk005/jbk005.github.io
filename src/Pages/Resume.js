f278e7481932907dd42c0e5439e00264375de0b7
jbk005 <jbharathkumar770@gmail.com>
2020-11-24 17:01:11 +0530

    pdf

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import ResumePDF from "../Assets/JBK_CV.pdf";
+import ResumePDF from "../Assets/logos/jbk_CV.pdf";
 
 class Resume extends Component {
     render() {
