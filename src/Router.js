3702f78a9ff07aada5b465da00eafecab8e120f5
JBK005 <jbharathkumar770@gmail.com>
2020-12-19 23:03:54 +0530

    contacticons&tweks

--- src/Router.js
+++ src/Router.js
@@ -21,7 +21,18 @@ const greeting = {
   /*  subTitle: emoji("I love finding out how things work and visualizing them in a creative way using computer technology🚀. I'm especially focused on python programming and also a mechanical product works."),*/
     resumeLink: "https://drive.google.com/file/d/1LUWb5ezb8xCgKDO5d2xG0K098EmR1z4j/view?usp=drivesdk"
   };
-  
+const socialMediaLinks = {
+
+    github: "https://github.com/jbk005/",
+    linkedin: "https://www.linkedin.com/in/jbk005/",
+    gmail: "jbharathkumar770@gmail.com",
+    instagram: "https://www.instagram.com/stranger_is_not_an_evil/",
+    facebook: "https://www.facebook.com/jbharath.kumar.005",
+    medium: "https://medium.com/@jbk005",
+    stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+    youtube: "https://www.youtube.com/c/JBK005"
+    // Instagram and Twitter are also supported in the links!
+  };  
 
 export default Router;
-export {greeting}
+export {greeting, socialMediaLinks}
--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -1,47 +1,97 @@
-import React, { Component } from "react";
+/* eslint-disable react/jsx-no-target-blank */
+
 import "./Contact.css";
-import { Container } from "react-bootstrap";
-import config from "../Configs/config.json";
-import { faFacebook,
-         faTwitter,
-         faInstagram,
-         faLinkedin,
-         faTwitch,
-         faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { withIsVisible } from "react-is-visible";
+import React from "react";
+import { socialMediaLinks } from "../Router";
 
-class Contact extends Component {
 
-    getSocialLink(type, link, icon) {
-        if (!link) {
-            return null;
-        }
-        
-        return <a href={link} title={type}><FontAwesomeIcon icon={icon} size="4x" /></a>
-    }
+export default function socialMedia() {
+  return (
+    <section id="Contact">
+    <div className="social-media-div">
+      
+      {socialMediaLinks.github ? 
+      (<a href={socialMediaLinks.github} className="icon-button github" target="_blank">
+        <i className="fab fa-github"></i>
+        <span></span>
+      </a>)
+      :
+      null}
 
-    render() {
-        const { isVisible } = this.props;
+      {socialMediaLinks.linkedin ?
+      <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank">
+        <i className="fab fa-linkedin-in"></i>
+        <span></span>
+      </a>
+      :
+      null}
 
-        return (
-            <section id="Contact">
-                <Container className={isVisible ? "Visible" : ""}>
-                    <h1 className="sectionTitle">Contact</h1>
-                    <p className="sectionContent">Feel free to send me a  <a href={`mailto:${config.email}`}>Email</a>, I'll try to respond as quick as I can!<br/>I'm also on these social media things, if you need me there:</p>
-                    
-                    <div className="SocialIcons">
-                        {this.getSocialLink("Facebook", config.socials.facebook, faFacebook)}
-                        {this.getSocialLink("Twitter", config.socials.twitter, faTwitter)}
-                        {this.getSocialLink("Instagram", config.socials.instagram, faInstagram)}
-                        {this.getSocialLink("LinkedIn", config.socials.linkedin, faLinkedin)}
-                        {this.getSocialLink("Twitch", config.socials.twitch, faTwitch)}
-                        {this.getSocialLink("YouTube", config.socials.twitch, faYoutube)}
-                    </div>
-                </Container>
-            </section>
-        )
-    }
+      {socialMediaLinks.gmail ?
+      <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank">
+        <i className="fab fa-google"></i>
+        <span></span>
+      </a>
+      :
+      null}
+
+      {socialMediaLinks.gitlab ?
+      // eslint-disable-next-line react/jsx-no-target-blank
+      <a href={socialMediaLinks.gitlab} className="icon-button gitlab" target="_blank">
+        <i className="fab fa-gitlab"></i>
+        <span></span>
+      </a>
+      :
+      null }
+
+      {socialMediaLinks.facebook ?
+      <a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank">
+        <i className="fab fa-facebook-f"></i>
+        <span></span>
+      </a>
+      :
+      null}
+
+       {socialMediaLinks.instagram ?
+      <a href={socialMediaLinks.instagram} className="icon-button instagram" target="_blank">
+        <i className="fab fa-instagram"></i>
+        <span></span>
+      </a>
+      :
+      null}
+
+      {socialMediaLinks.twitter ?
+      <a href={socialMediaLinks.twitter} className="icon-button twitter" target="_blank">
+        <i className="fab fa-twitter"></i>
+        <span></span>
+      </a>
+      :
+      null}
+
+      {socialMediaLinks.medium ? 
+      (<a href={socialMediaLinks.medium} className="icon-button medium" target="_blank">
+        <i className="fab fa-medium"></i>
+        <span></span>
+      </a>)
+      :
+      null}
+
+      {socialMediaLinks.stackoverflow ? 
+      (<a href={socialMediaLinks.stackoverflow} className="icon-button stack-overflow" target="_blank">
+        <i className="fab fa-stack-overflow"></i>
+        <span></span>
+      </a>)
+      :
+      null}
+
+      {socialMediaLinks.youtube ? 
+      (<a href={socialMediaLinks.youtube} className="icon-button youtube" target="_blank">
+        <i className="fab fa-youtube"></i>
+        <span></span>
+      </a>)
+      :
+      null}
+
+    </div>
+    </section>
+  );
 }
-
-export default withIsVisible(Contact);
