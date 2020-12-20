fc6245409de1b607cc9609796bed09a20c550d83
JBK005 <jbharathkumar770@gmail.com>
2020-12-20 08:33:09 +0530

    removed one twitter

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -59,14 +59,6 @@ export default function socialMedia() {
       :
       null}
 
-      {socialMediaLinks.twitter ?
-      <a href={socialMediaLinks.twitter} className="icon-button twitter" target="_blank">
-        <i className="fab fa-twitter"></i>
-        <span></span>
-      </a>
-      :
-      null}
-
       {socialMediaLinks.medium ? 
       (<a href={socialMediaLinks.medium} className="icon-button medium" target="_blank">
         <i className="fab fa-medium"></i>
